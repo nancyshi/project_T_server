9ba95412197d40985a62944284fdcd4471ba9b7a
Nancyshi <nancyshi@foxmail.com>
2020-01-07 15:49:56 +0800

    bug fix

--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,18 @@
 import tornado.ioloop
 import tornado.web
 import os
-
-from login import LoginHandler
-from dataMgr import DataMgrHandler
 path = os.sys.path[0]
 os.chdir(path)
+from login import LoginHandler
+from dataMgr import DataMgrHandler
+
+
 
 app = tornado.web.Application([
     (r"/login",LoginHandler),
     (r"/data",DataMgrHandler)
 ])
-
+print (path)
 
 if __name__ == "__main__":
     app.listen(8888)
